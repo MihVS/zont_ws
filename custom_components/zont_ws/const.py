92f0187437e85d6209809863cd288ce8679578c6
mihvs <mihvs22@gmail.com>
2026-01-05 23:11:55 +0300

    ZONT: zont websocket api

---
+++ custom_components/zont_ws/const.py
@@ -0,0 +1,46 @@
+DOMAIN = 'zont_ws'
+MANUFACTURER = 'MicroLine'
+
+WS_TIMEOUT_REQUEST = 10
+HEARTBEAT = 60
+
+WS_KEY_USER = 'user'
+WS_KEY_PASS = 'pass'
+WS_KEY_AUTH = 'auth'
+WS_AUTH_SUCCESS = 200
+WS_AUTH_ERROR = 401
+WS_KEY_REQUEST_IDS = 'req_ids'
+WS_KEY_IDS = 'ids'
+WS_KEY_ID = 'id'
+WS_KEY_RESPONSE_ID = 'Id'
+WS_KEY_REQUEST_STATE = 'req_state'
+WS_KEY_TYPE = 'type'
+WS_KEY_NAME = 'name'
+WS_KEY_STYPE = 'stype'
+WS_KEY_CMD = 'cmd'
+WS_KEY_CMD_RESULT = 'cmdres'
+WS_KEY_SERVICE_CMD = 'scmd'
+WS_KEY_SERVICE_CMD_RESULT = 'scmdres'
+
+ZONT_TYPE_ANALOG_INPUT = 0
+ZONT_TYPE_DS18_TEMP_SENSOR = 1
+ZONT_TYPE_SECURITY_ZONE = 2
+ZONT_TYPE_CSH_ADAPTER = 6
+ZONT_TYPE_RADIO_SENSOR = 8
+ZONT_TYPE_WEB_ELEMENT = 10
+ZONT_TYPE_RELAY = 14
+ZONT_TYPE_MIXER = 15
+ZONT_TYPE_HEATING_CIRCUIT = 16
+ZONT_TYPE_PUMP = 17
+ZONT_TYPE_NTC_TEMP_SENSOR = 27
+ZONT_TYPE_ANY = 255
+
+PLATFORMS = [
+    'sensor',
+    'binary_sensor',
+    'switch',
+    'button',
+    'climate',
+    'alarm_control_panel',
+    'device_tracker'
+]
--- custom_components/zont_ws/core/zont_ws_api.py
+++ custom_components/zont_ws/core/zont_ws_api.py
@@ -3,6 +3,12 @@ import logging
 from typing import Any
 
 import aiohttp
+
+from ..const import (
+    WS_TIMEOUT_REQUEST, HEARTBEAT, WS_KEY_USER, WS_KEY_AUTH,
+    WS_KEY_REQUEST_IDS, WS_KEY_IDS, WS_KEY_ID, WS_KEY_REQUEST_STATE,
+    WS_KEY_CMD, WS_KEY_SERVICE_CMD, WS_KEY_SERVICE_CMD_RESULT
+)
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 _LOGGER = logging.getLogger(__name__)
@@ -34,42 +40,45 @@ class ZontWsApi:
         if self._connected:
             return
 
-        _LOGGER.debug("Connecting to ZONT WS: %s", self._host)
+        _LOGGER.debug(f'Connecting to ZONT WS: {self._host}')
 
         try:
             self._ws = await self._session.ws_connect(
                 url= self._host,
-                heartbeat=30,
-                timeout=10,
+                heartbeat=HEARTBEAT,
+                timeout=WS_TIMEOUT_REQUEST,
             )
 
             await self._ws.send_json(
                 {
-                    "user": self._user,
-                    "pass": self._password,
+                    WS_KEY_USER: self._user,
+                    WS_KEY_AUTH: self._password,
                 }
             )
 
             msg = await self._ws.receive(timeout=10)
 
             if msg.type != aiohttp.WSMsgType.TEXT:
-                raise ZontWsError("Invalid auth response")
+                raise ZontWsError(f'Invalid auth response. Host: {self._host}')
 
             data = msg.json()
-            if data.get("auth") != 200:
-                raise ZontAuthError("ZONT authentication failed")
+            if data.get(WS_KEY_AUTH) != 200:
+                raise ZontAuthError(f'ZONT authentication failed! '
+                                    f'Host: {self._host}')
 
             self._connected = True
-            _LOGGER.debug("ZONT WS connected and authorized")
+            _LOGGER.debug(f'ZONT WS connected and authorized. '
+                          f'Host: {self._host}')
 
         except Exception as err:
             await self.close()
-            raise ZontWsError(f"WS connect failed: {err}") from err
+            raise ZontWsError(f'WS connect failed: {err}. '
+                              f'Host: {self._host}') from err
 
     async def close(self) -> None:
         """Close websocket."""
         if self._ws is not None:
-            _LOGGER.debug("Closing ZONT WS")
+            _LOGGER.debug(f'Closing ZONT WS. Host: {self._host}')
             try:
                 await self._ws.close()
             except Exception:
@@ -79,51 +88,48 @@ class ZontWsApi:
         self._connected = False
 
     async def request(self, payload: dict[str, Any]) -> dict[str, Any]:
-        """
-        Send request and wait for response.
-        Ensures:
-        - single request at a time
-        - reconnect on failure
-        """
+        """Send request and wait for response."""
         async with self._lock:
             if not self._connected:
                 await self.connect()
 
             try:
-                _LOGGER.debug("ZONT WS → %s", payload)
+                _LOGGER.debug(f'ZONT WS → {payload}')
                 await self._ws.send_json(payload)
 
                 msg = await self._ws.receive(timeout=15)
 
                 if msg.type == aiohttp.WSMsgType.TEXT:
                     data = msg.json()
-                    _LOGGER.debug("ZONT WS ← %s", data)
+                    _LOGGER.debug(f'ZONT WS ← {data}')
                     return data
 
                 if msg.type in (
                     aiohttp.WSMsgType.CLOSE,
                     aiohttp.WSMsgType.ERROR,
                 ):
-                    raise ZontWsError("WebSocket closed")
+                    raise ZontWsError(f'WebSocket closed. Host: {self._host}')
 
-                raise ZontWsError(f"Unexpected WS msg: {msg.type}")
+                raise ZontWsError(f'Unexpected WS msg: {msg.type}. '
+                                  f'Host: {self._host}')
 
             except Exception as err:
-                _LOGGER.warning("ZONT WS error, reconnect needed: %s", err)
+                _LOGGER.warning(f'ZONT WS error (host: {self._host}), '
+                                f'reconnect needed: {err}')
                 await self.close()
                 raise
 
     async def get_ids(self, obj_type: int = 255) -> list[int]:
         """Request list of object IDs."""
-        data = await self.request({"req_ids": obj_type})
-        return data.get("ids", [])
+        data = await self.request({WS_KEY_REQUEST_IDS: obj_type})
+        return data.get(WS_KEY_IDS, [])
 
     async def get_state(self, obj_id: int) -> dict[str, Any]:
         """Request object state."""
         return await self.request(
             {
-                "id": obj_id,
-                "req_state": 0,
+                WS_KEY_ID: obj_id,
+                WS_KEY_REQUEST_STATE: 0,
             }
         )
 
@@ -131,12 +137,12 @@ class ZontWsApi:
         """Send command to object."""
         return await self.request(
             {
-                "id": obj_id,
-                "cmd": cmd,
+                WS_KEY_ID: obj_id,
+                WS_KEY_CMD: cmd,
             }
         )
 
     async def send_system_command(self, scmd: str) -> str:
         """Send system command (#S7?, etc)."""
-        data = await self.request({"scmd": scmd})
-        return data.get("scmdres", "")
+        data = await self.request({WS_KEY_SERVICE_CMD: scmd})
+        return data.get(WS_KEY_SERVICE_CMD_RESULT, '')
